c6918a2f3dea58e7cec607141514bbf7cfb0c5f0
MDrenova <mateodrenova@gmail.com>
2024-04-22 11:59:47 +0200

    Update: comment

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-"""annotations - floor"""
+"""
+annotations - floor
+"""
 
 
 a: int = 1
-pi:float = 3.14
+pi: float = 3.14
 i_understand_annotations: bool = True
 school: str = "Holberton"
