6e14da1b22cb3c24e8f9551c47abd61dd1d5d9d1
MDrenova <mateodrenova@gmail.com>
2024-04-24 12:26:41 +0200

    Update: pycodestyle

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -9,11 +9,10 @@ import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
-    """
-    Coroutine that executes async_comprehension four times in parallel,
-    measures the total runtime, and returns it.
-    """
+async def measure_runtime() -> float:
+    '''
+    should measure the total runtime and return it
+    '''
     start = time.time()
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
     end = time.time()
