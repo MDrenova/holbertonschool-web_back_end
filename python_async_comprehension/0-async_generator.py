d89754799d124c3e4127940601cb39517084f7f3
MDrenova <mateodrenova@gmail.com>
2024-04-24 12:23:43 +0200

    Update: pycodestyle

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 import asyncio
 import random
+from typing import Generator
 '''
 Python - Async Comprehension
 '''
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     Coroutine that asynchronously waits for 1 second,
     then yields a random number between 0 and 10.
