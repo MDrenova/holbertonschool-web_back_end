084ce6a58794b60e4bc98d8987036a5dc8c12cc1
MDrenova <mateodrenova@gmail.com>
2024-04-24 11:56:07 +0200

    Add async_generator coroutine

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    Coroutine that asynchronously waits for 1 second,
+    then yields a random number between 0 and 10.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)  # Asynchronously wait for 1 second
+        yield random.randint(0, 10)  # Yield a random number between 0 and 10
