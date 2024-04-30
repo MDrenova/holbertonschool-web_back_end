c1f225806fde41178bb549b814b7f60fef7f647f
MDrenova <mateodrenova@gmail.com>
2024-04-30 13:44:48 +0200

    Update: code

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -3,10 +3,10 @@
 NoSQL
 """
 
-def update_topics(mongo_collection, name, topics):
+
+def update_topics(mongo_collection, topic):
     """
     function that changes all topics of a school document based on the name
     """
-    myquery = {"name": name}
-    newvalues = {"$set": {"topics": topics}}
-    mongo_collection.update_many(myquery, newvalues)
+    value = {"topics": topic}
+    return mongo_collection.find(value)
