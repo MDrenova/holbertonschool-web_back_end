9b14dcd8c1d16f5e7171a082eb213508cf2be2dc
MDrenova <mateodrenova@gmail.com>
2024-05-20 19:35:42 +0200

    Add a function without arrow function

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -2,3 +2,10 @@
 const getStudentIdsSum = (list) => list.reduce((total, student) => total + student.id, 0);
 
 export default getStudentIdsSum;
+// function getStudentIdsSum(list) {
+//   var total = 0;
+//   for (var i = 0; i < list.length; i++) {
+//       total = total + list[i].id;
+//   }
+//   return total;
+// }
