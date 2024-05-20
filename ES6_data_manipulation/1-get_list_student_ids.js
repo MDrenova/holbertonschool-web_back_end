24dbc0adbab81df1fd54c31279edddf1ee0a0b6f
MDrenova <mateodrenova@gmail.com>
2024-05-20 13:55:50 +0200

    Update: ESLint

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,4 +1,4 @@
 export default function getListStudentIds(array) {
-    if (!Array.isArray(array)) return [];
-    return array.map((el) => el.id);
-  }
+  if (!Array.isArray(array)) return [];
+  return array.map((el) => el.id);
+}
